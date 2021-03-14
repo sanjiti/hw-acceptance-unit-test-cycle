8e1874f62a59e09b0b24986f1025dd74284a52fd
EC2 Default User <ec2-user@ip-172-31-42-23.us-east-2.compute.internal>
2021-03-14 23:10:44 +0000

    added files from HW3

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ActiveRecord::Base
+    def self.all_ratings
+    %w(G PG PG-13 NC-17 R)
+    end
+    
     def others_by_same_director(director)
         Movie.where(director: director)
     end
